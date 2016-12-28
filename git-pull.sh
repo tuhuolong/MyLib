8b18f9b2d626805c308a7100323b58f3609a8f90
tuhuolong <tuhuolong@gmail.com>
2016-12-28 18:08:53 +0800

    add git-pull.sh
    Signed-off-by: tuhuolong <tuhuolong@gmail.com>

---
+++ git-pull.sh
@@ -0,0 +1,8 @@
+git clone https://github.com/tuhuolong//asynccallback.git
+git clone https://github.com/tuhuolong//banner.git
+git clone https://github.com/tuhuolong//common.git
+git clone https://github.com/tuhuolong//common-ui.git
+git clone https://github.com/tuhuolong//nestedscroll.git
+git clone https://github.com/tuhuolong//plugin-frame.git
+git clone https://github.com/tuhuolong//plugin-sdk.git
+git clone https://github.com/tuhuolong//tabfragment.git
