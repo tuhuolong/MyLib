f551d1b1a77baceb8a92c922bdb7604ac836d306
tuhuolong <tuhuolong@gmail.com>
2017-04-14 16:10:41 +0800

    add something
    Signed-off-by: tuhuolong <tuhuolong@gmail.com>

--- git-init-pull.sh
+++ git-init-pull.sh
@@ -4,3 +4,4 @@ git clone https://github.com/tuhuolong/common-ui.git
 git clone https://github.com/tuhuolong/plugin-frame.git
 git clone https://github.com/tuhuolong/plugin-sdk.git
 git clone https://github.com/tuhuolong/tabfragment.git
+Creating a new branch is quick & simple.
