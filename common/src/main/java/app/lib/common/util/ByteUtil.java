0eebd94551521a04dc40efcee1acbc5fb28d9856
tuhuolong <tuhuolong@gmail.com>
2017-06-27 17:52:32 +0800

    update
    Signed-off-by: tuhuolong <tuhuolong@gmail.com>

--- common/src/main/java/app/lib/common/util/ByteUtil.java
+++ common/src/main/java/app/lib/common/util/ByteUtil.java
@@ -258,4 +258,14 @@ public class ByteUtil {
             }
         }
     }
+
+    public static byte[] concat(byte[] first, byte[] second) {
+        byte[] result = new byte[first.length + second.length];
+
+        System.arraycopy(first, 0, result, 0, first.length);
+
+        System.arraycopy(second, 0, result, first.length, second.length);
+
+        return result;
+    }
 }
